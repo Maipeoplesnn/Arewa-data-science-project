1a9d32c8534b3cae1c87365335b185b37bf127ea
Abdullahi Maijamaa Yunusa <maipeople4008@gmail.com>
2025-01-26 11:26:04 +0100

    notebook

--- project.py
+++ project.py
@@ -3,11 +3,7 @@ import requests
 API_KEY = '3aaa575bac929632e1e00cf301565205'
 city = 'Kano'
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
-
 response = requests.get(url)
-data = response.json()
-
-print(data)  # Print the data to see the structure
 data = response.json()
 
 print(data)  # Print the data to see the structure
