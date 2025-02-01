65774c8626e0d6edf20f37c7f9db7c1e9519fec9
Abdullahi Maijamaa Yunusa <maipeople4008@gmail.com>
2025-02-01 12:06:40 +0100

    py extension

--- project.py
+++ project.py
@@ -1,8 +1,9 @@
 import requests
 
 API_KEY = '3aaa575bac929632e1e00cf301565205'
-city = 'Kano'
+city = input ('Enter city you want see their weather')
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+
 response = requests.get(url)
 data = response.json()
 
